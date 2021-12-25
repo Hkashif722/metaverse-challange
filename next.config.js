9d1c106164769ede5b3e220eadce3a0f94117dc6
Chocovanilla7 <kashifh287@gmail.com>
2021-12-25 17:37:54 +0530

    login

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: [
+      "avatars.dicebear.com",
+      "images.theconversation.com",
+      "links.papareact.com",
+    ],
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Login from "../components/Login";
+import { useMoralis } from "react-moralis";
+
+export default function Home() {
+  const { isAuthenticated, logout } = useMoralis();
+  console.log(isAuthenticated);
+  if (!isAuthenticated) return <Login />;
+  return (
+    <div className=" ">
+      <Head>
+        <title>Metaverse Challange</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome to the App</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
